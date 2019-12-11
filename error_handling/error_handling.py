a669808dbf264eea10ab1fce4b4a8a0f31d35b2b
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-12-10 21:33:23 -0800

    added error handling

---
+++ error_handling/error_handling.py
@@ -0,0 +1,28 @@
+# error handling #1
+while True:
+    try: 
+        age = int(input('What is your age?'))
+        10/age
+        print(age)
+        # raise Exception('Hey cut it out')
+        # raise ValueError('Hey cut it out2')
+    except ValueError: 
+        print("please enter a number")
+    except ZeroDivisionError: 
+        print("please enter a number > 0")
+    else:
+        print('thank you!')
+        break
+    finally: 
+        print('ok, i am finally done')
+
+# error handling #2
+def sum(num1, num2):
+    try: 
+        return num1/num2
+#    except TypeError as err:
+    except (TypeError, ZeroDivisionError) as err:
+        print(f"enter numbers {err}")
+
+print(sum('1', 2))
+
