d936a1cf570271fc2c7091e7d5c63dac516ec988
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-12-29 15:31:30 -0800

    added variable rules

--- py_projects/web_development/web_server/server.py
+++ py_projects/web_development/web_server/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, escape
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', name='marvin')
 
 
 @app.route('/blog')
@@ -20,6 +21,32 @@ def blog_f2():
 @app.route('/about.html')
 def about():
     return render_template('about.html')
+
+# Variable rules:
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/?highlight=variable%20rule
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User: %s' % escape(username)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/user/<username>/<int:post_id>')
+def show_user_post_id(username, post_id):
+    # show the user profile for that user
+    return 'User: %s' % escape(username) + '<br> post_id: %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
 
 # Catch all routes
 @app.route('/', defaults={'path': ''})
