83e16a3654be2669a826df01c6b6ad3f33aa337a
Marvin <44047272+marvinxu99@users.noreply.github.com>
2020-02-23 20:40:39 -0800

    added os.environ examples

---
+++ os.environ/py_environ.py
@@ -0,0 +1,32 @@
+# Python program to explain os.environ object 
+
+# importing os module 
+import os 
+import pprint 
+
+# Get the list of user's 
+# environment variables 
+env_var = os.environ 
+
+# Print the list of user's 
+# environment variables 
+print("User's Environment variable:") 
+pprint.pprint(dict(env_var), width = 1) 
+
+print("OS:", end=' ') 
+pprint.pprint(env_var.get('OS'))
+
+print("HOMEDRIVE:", end=' ') 
+pprint.pprint(env_var['HOMEDRIVE'])
+
+if 'HOME' in env_var:
+	print("'HOME' is included in os.environ")
+else:
+	print("'HOME' is not included in os.environ")
+
+if 'HOMEDRIVE' in env_var:
+	print("'HOMEDRIVE' is included in os.environ")
+else:
+	print("'HOMEDRIVE' is not included in os.environ")
+	
+
