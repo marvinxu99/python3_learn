31bf728a9aa64d989b70aaf59651c3a22a2de7d7
Marvin Xu <marvinxu99@hotmail.com>
2020-07-01 21:42:29 -0700

    added mail.py

---
+++ mail.py
@@ -0,0 +1,10 @@
+# import os,time
+# A='%-99o'%int('t12q2lxqkap48euoej9429cstbnazl63ubyryteo49u',36)
+# for i in'0123456':
+ # t=os.fork()
+ # while t<1:T=int(time.time())%50;(time.sleep,(id,os.urandom)[i<A[T+49]])[i<A[T]](1)
+ 
+from time import*
+while 1:
+ sleep((ord('002464200~~A5005A~~00~~00DDD~~'[int(time()/2)%30])-48)*0.001);x=10**5
+ while x:x-=1
