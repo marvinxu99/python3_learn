e28c3bf68b437e4ff6da391f0eb726eeda490a36
Marvin Xu <marvinxu99@hotmail.com>
2021-06-14 18:59:45 -0700

    start what XML is

---
+++ 1_XML/essential training/xml_intro.py
@@ -0,0 +1,45 @@
+'''
+install XML tools for visual studio code.
+
+XML = eXtensible Markup Language
+
+XML is the foundation of several Web technologies:
+- XHTML - HTML formatted as XML syntax
+- RSS/ATOM - used for publishing, such as blogs
+- AJAX - asynchronous JavaScript XML
+- Web services 
+
+What is XML used for?
+- XML is used to structure and describe information
+- XML was intended to be used over the internet
+- used to exchange data between disparate systems
+
+XML-Related technologes:
+- XPath - eXtensible Path Langauge
+- XSLT - XML Stylesheet Language Transformations
+- XQuery - more advanced querying language than XPath
+- Xpointer, XLink - links between and within XMLL documents
+
+Describing Data with XML
+<BusinessCard>
+<name>Marvin Xu</name>
+<phone type="home">123444</phone>
+<phone type="work">123444</phone>
+<phone type="mobile">123444</phone>
+<email>test@test.com</email>
+</BusinessCard>
+
+Advantage of XML
+- XML keeps content separate from presentation
+- XML is an open format that can be ready by many apps
+- XML can be used on both the client and the server
+- XML has widespread support in multiple languages and runtimes
+- XML to communicate between different systems
+
+Disadvantage of XML:
+- XML is not suitable for very large data sets  (data sets >= MB)
+
+
+
+
+''''
