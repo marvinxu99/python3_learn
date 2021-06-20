a0ce107a5c70a6e5eabd1a4eb4215a22cb3270d8
marvinxu99 <marvinxu99@hotmail.com>
2021-06-20 11:41:45 -0700

    file util

--- __linkedin_learning__/learning python/Ch4/files_start.py
+++ __linkedin_learning__/learning python/Ch4/files_start.py
@@ -5,19 +5,24 @@
 def main():  
   # Open a file for writing and create it if it doesn't exist
   f = open("textfile.txt", "w+")
-
-  
-  # Open the file for appending text to the end
   for i in range(10):
     f.write("This is line " + str(i) + "\n")
-
-  # write some lines of data to the file
-
-  
-  # close the file when done
   f.close()
-  
+
+  # Open the file for appending text to the end
+  f = open("textfile.txt", "a")
+  for i in range(10):
+    f.write("This is line " + str(i) + "\n")
+  f.close()
+ 
   # Open the file back up and read the contents
+  f = open("textfile.txt", "r")
+  if f.mode == 'r':
+    # content = f.read()
+    # print(content)
+    fl = f.readlines()
+    for x in fl:
+      print(x)
 
     
 if __name__ == "__main__":
