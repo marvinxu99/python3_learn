cfac4a5717bfb5e7bc11ff578f983f480b28b09f
marvinxu99 <marvinxu99@hotmail.com>
2021-06-20 14:24:33 -0700

    added html parser example

--- __linkedin_learning__/learning python/Ch5/htmlparsing_start.py
+++ __linkedin_learning__/learning python/Ch5/htmlparsing_start.py
@@ -1,12 +1,61 @@
 # 
 # Example file for parsing and processing HTML
 #
+from html.parser import HTMLParser
+from typing import List, Tuple, Optional
+
+metacount = 0
+
+class MyHTMLParser(HTMLParser):
+  # def handle_comment(self, data):
+  def handle_comment(self, data: str) -> None:
+    print("Encountered comment: ", data)
+    pos = self.getpos()
+    print("\t At line: ", pos[0], " position ", pos[1])
+    return super().handle_comment(data)
+
+  # def handle_starttag(self, tag, attrs) -> None:
+  def handle_starttag(self, tag: str, attrs: List[Tuple[str, Optional[str]]]) -> None:
+    global metacount
+    if tag == "meta":
+      metacount += 1
+    print("Encountered tag: ", tag)
+    pos = self.getpos()
+    print("\t At line: ", pos[0], " position ", pos[1])
+
+    if attrs.__len__() > 0: 
+      print("\tAttributes:")
+      for a in attrs:
+        print("\t", a[0], "=", a[1])
+
+    return super().handle_starttag(tag, attrs)
+
+
+  def handle_endtag(self, tag: str) -> None:
+    print("Encountered tag: ", tag)
+    pos = self.getpos()
+    print("\tAt line: ", pos[0], " position ", pos[1])
+    return super().handle_endtag(tag)
+
+
+  def handle_data(self, data: str) -> None:
+    if data.isspace(): 
+      return
+    print("Encountered data: ", data)
+    pos = self.getpos()
+    print("\t At line: ", pos[0], " position ", pos[1])
+    return super().handle_data(data)
 
 
 def main():
   # instantiate the parser and feed it some HTML
   parser = MyHTMLParser()
-    
+  with open('samplehtml.html') as f:
+    contents = f.read()
+    parser.feed(contents)
+  
+  print("Total meta tags found:", metacount)
+
 
 if __name__ == "__main__":
   main();
--- __linkedin_learning__/learning python/Ch5/jsondata_start.py
+++ __linkedin_learning__/learning python/Ch5/jsondata_start.py
@@ -1,25 +1,43 @@
 # 
 # Example file for parsing and processing JSON
 #
+#https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+#
+#
 import urllib.request 
+import json
 
 def printResults(data):
   # Use the json module to load the string data into a dictionary
   theJSON = json.loads(data)
   
   # now we can access the contents of the JSON like any other Python object
-
+  if "title" in theJSON['metadata']:
+    print("Title:", theJSON['metadata']['title']) 
   
   # output the number of events, plus the magnitude and each event name  
-
+  count = theJSON['metadata']['count']
+  print(count, "events recorded")
   
   # for each event, print the place where it occurred
-
+  for event in theJSON['features']:
+    print(event['properties']['mag'], event['properties']['place'])
+  print("-----------")
 
   # print the events that only have a magnitude greater than 4
-
+  for event in theJSON['features']:
+    if event['properties']['mag'] > 4:
+      print(event['properties']['mag'], event['properties']['place'])
+#      print("%2.1f" % event['properties']['mag'], event['properties']['place'])
+  print("-------------------")
 
   # print only the events where at least 1 person reported feeling something
+  print("Events that were felt:")
+  for event in theJSON['features']:
+    feltReports = event['properties']['felt']
+    if  feltReports != None and feltReports > 0: 
+      print(event['properties']['mag'], event['properties']['place'], 
+            f" reported { feltReports } times")
 
   
 def main():
@@ -33,7 +51,7 @@ def main():
   print ("result code: " + str(webUrl.getcode()))
   if (webUrl.getcode() == 200):
     data = webUrl.read()
-    print(data)
+    printResults(data)
   else:
     print("Received error, cannot parsr results.")
 
