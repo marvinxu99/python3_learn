f4a444f3a2de26774e94ac8646e2844031da61d3
marvinxu99 <marvinxu99@hotmail.com>
2021-06-23 16:51:21 -0700

    dict examples

--- __linkedin_learning__/python_essential_training/Chap08/dict.py
+++ __linkedin_learning__/python_essential_training/Chap08/dict.py
@@ -4,9 +4,17 @@
 def main():
     animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
         'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
+    
+    animals2 = dict(kitten = 'meow', puppy = 'ruff!', 
+                lion = 'grrr', giraffe = 'I am a giraffe!', 
+                dragon = 'rawr' )
+
     print_dict(animals)
+    print("--------------")
+    print_dict(animals2)
 
 def print_dict(o):
     for x in o: print(f'{x}: {o[x]}')
+
 
 if __name__ == '__main__': main()
