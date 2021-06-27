b93bffa4fa53dcc701c3e02df2f96022319b2bef
marvinxu99 <marvinxu99@hotmail.com>
2021-06-26 22:48:31 -0700

    built in functions

---
+++ f_string and String Object/templstr_start.py
@@ -0,0 +1,26 @@
+# demonstrate template string functions
+
+from string import Template
+
+def main():
+    # Usual string formatting with format()
+    str1 = "You're watching {0} by {1}".format("Advanced Python", "Joe Marini")
+    print(str1)
+    
+    # TODO: create a template with placeholders
+    templ = Template('You\'re watching ${title} by ${author}')
+
+    # TODO: use the substitute method with keyword arguments
+    str2 = templ.substitute(title='Advanced Python', author='Jeo Marini')
+    print(str2)
+
+    # TODO: use the substitute method with a dictionary
+    data = {
+        'author': 'Jeo Marini',
+        'title': 'Advanced Python' 
+    }
+    str3 = templ.substitute(data)
+    print(str3)
+if __name__ == "__main__":
+    main()
+    
---
+++ iter and itertools/iterators_start.py
@@ -0,0 +1,34 @@
+# use iterator functions like enumerate, zip, iter, next
+
+
+def main():
+    # define a list of days in English and French
+    days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
+
+    # TODO: use iter to create an iterator over a collection
+    i = iter(days)
+    print(next(i))
+    print(next(i))
+    print(next(i))
+
+    # TODO: iterate using a function and a sentinel
+    with open('testfile.txt', "r") as fp:
+        for line in iter(fp.readline, ''):
+            print(line.rstrip())
+
+    # TODO: use regular interation over the days
+    # for i in range(len(days)):
+    #     print(i, days[i])
+
+    # TODO: using enumerate reduces code and provides a counter
+    for i, m in enumerate(days, start=1):
+        print(i, m)
+
+    # TODO: use zip to combine sequences
+    for i in enumerate(zip(days, daysFr)):
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
