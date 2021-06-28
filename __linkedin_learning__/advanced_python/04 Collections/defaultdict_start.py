2048bcbc8eded995a836b6d30e09c3db816667a3
marvinxu99 <marvinxu99@hotmail.com>
2021-06-27 17:57:43 -0700

    defaultdict example

--- __linkedin_learning__/advanced_python/04 Collections/defaultdict_start.py
+++ __linkedin_learning__/advanced_python/04 Collections/defaultdict_start.py
@@ -1,5 +1,6 @@
 # Demonstrate the usage of defaultdict objects
 
+from collections import defaultdict
 
 def main():
     # define a list of items that we want to count
@@ -7,16 +8,42 @@ def main():
               'apple', 'grape', 'banana', 'banana']
 
     # use a dictionary to count each element
-    fruitCounter = {}
-
     # Count the elements in the list
+    fruitCounter = {}
     for fruit in fruits:
-        fruitCounter[fruit] += 1
+        if fruit in fruitCounter.keys():
+            fruitCounter[fruit] += 1
+        else:
+            fruitCounter[fruit] = 1
+
+    # use defaultdict
+    fruitCounter2 = defaultdict(int)   # int is the factory
+    for fruit in fruits:
+        fruitCounter2[fruit] += 1
 
     # print the result
     for (k, v) in fruitCounter.items():
         print(k + ": " + str(v))
+    print('----------')
+    for (k, v) in fruitCounter2.items():
+        print(k + ": " + str(v))
 
+    # Use a lambda function for the factory
+    fruitCounter3 = defaultdict(lambda: 100)   # starting from 100
+    for fruit in fruits:
+        fruitCounter3[fruit] += 1
+    # print the result
+    print('----------')
+    for (k, v) in fruitCounter3.items():
+        print(k + ": " + str(v))
+
+    # Another example
+    s = 'mississippi'
+    d = defaultdict(int)
+    for k in s:
+        d[k] += 1
+    print(sorted(d.items())
+)
 
 if __name__ == "__main__":
     main()
--- __linkedin_learning__/advanced_python/04 Collections/namedtuple_start.py
+++ __linkedin_learning__/advanced_python/04 Collections/namedtuple_start.py
@@ -5,9 +5,18 @@ import collections
 
 def main():
     # TODO: create a Point namedtuple
+    Point = collections.namedtuple('Point', 'x y')
+    print(type(Point))
+    p1 = Point(10, 20)
+    p2 = Point(30, 40)
+    print(p1, p2)
+    print(p1.x, p1.y)
 
     # TODO: use _replace to create a new instance
-    pass
+    print(id(p1))
+    p1 = p1._replace(x=100)
+    print(id(p1))
+    print(p1)
 
 
 if __name__ == "__main__":
