d24a92d7fa6f4d408aaccce0ed55ac0aec690510
marvinxu99 <marvinxu99@hotmail.com>
2021-06-27 22:03:12 -0700

    counter example

--- __linkedin_learning__/advanced_python/04 Collections/counter_start.py
+++ __linkedin_learning__/advanced_python/04 Collections/counter_start.py
@@ -1,4 +1,18 @@
 # Demonstrate the usage of Counter objects
+'''
+c = Counter(a=3, b=1)
+d = Counter(a=1, b=2)
+c + d                       # add two counters together:  c[x] + d[x]
+Counter({'a': 4, 'b': 3})
+c - d                       # subtract (keeping only positive counts)
+Counter({'a': 2})
+c & d                       # intersection:  min(c[x], d[x]) 
+Counter({'a': 1, 'b': 1})
+c | d                       # union:  max(c[x], d[x])
+Counter({'a': 3, 'b': 2})
+
+'''
+
 
 from collections import Counter
 
