2a4710b3f62be1356e5a2ce4da37a69f1c9e7dd4
marvinxu99 <marvinxu99@hotmail.com>
2021-06-28 14:20:39 -0700

    __str__

--- __linkedin_learning__/advanced_python/05 Classes/objectstrs_start.py
+++ __linkedin_learning__/advanced_python/05 Classes/objectstrs_start.py
@@ -1,5 +1,10 @@
 # customize string representations of objects
-
+'''
+object.__str()__
+object.__repr()__
+object.__format()__
+object.__bytes()__
+'''
 
 class Person():
     def __init__(self):
@@ -8,8 +13,12 @@ class Person():
         self.age = 25
 
     # TODO: use __repr__ to create a string useful for debugging
+    def __repr__(self) -> str:
+        pass
 
     # TODO: use str for a more human-readable string
+    def __str__(self) -> str:
+        pass
 
 
 def main():
