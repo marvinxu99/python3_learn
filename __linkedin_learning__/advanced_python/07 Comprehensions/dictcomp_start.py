1cde5432c8671c4bdb316f52a877908ff3e21eb7
marvinxu99 <marvinxu99@hotmail.com>
2021-06-30 17:01:43 -0700

    comprehension

--- __linkedin_learning__/advanced_python/07 Comprehensions/dictcomp_start.py
+++ __linkedin_learning__/advanced_python/07 Comprehensions/dictcomp_start.py
@@ -6,10 +6,15 @@ def main():
     ctemps = [0, 12, 34, 100]
 
     # TODO: Use a comprehension to build a dictionary
+    tempDict = {t: (t*9/5 + 32) for t in ctemps if t < 100}
+    print(tempDict)
 
     # TODO: Merge two dictionaries with a comprehension
     team1 = {"Jones": 24, "Jameson": 18, "Smith": 58, "Burns": 7}
     team2 = {"White": 12, "Macke": 88, "Perce": 4}
+
+    newTeam = {k:v for team in (team1, team2) for k,v in team.items() }
+    print(newTeam)
 
 
 if __name__ == "__main__":
