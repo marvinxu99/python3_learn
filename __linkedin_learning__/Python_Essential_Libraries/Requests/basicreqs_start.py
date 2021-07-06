b9afb63f7534536a07600f627c87628711801777
marvinxu99 <marvinxu99@hotmail.com>
2021-07-06 07:52:25 -0700

    started the requests module

--- __linkedin_learning__/Python_Essential_Libraries/Requests/basicreqs_start.py
+++ __linkedin_learning__/Python_Essential_Libraries/Requests/basicreqs_start.py
@@ -3,9 +3,15 @@
 import requests
 
 # TODO: create a basic request for data
-
+# resp = requests.get("http://httpbin.org/xml")
+# print(resp.status_code)
+# print(resp.text)
 
 # TODO: create a request using parameters
+args = {"key1": 1, "key2": 'two', 'key3': False}
+resp = requests.get("http://httpbin.org/get", params=args)
+print(resp.status_code)
+print(resp.text)
 
 
 # TODO: create a request using POST
