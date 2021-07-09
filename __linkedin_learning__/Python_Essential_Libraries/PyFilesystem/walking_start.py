1b42e44e99cc3320ee51a916b63732a92a56b445
marvinxu99 <marvinxu99@hotmail.com>
2021-07-08 18:07:46 -0700

    completed PyFilesystem examples

--- __linkedin_learning__/Python_Essential_Libraries/PyFilesystem/walking_start.py
+++ __linkedin_learning__/Python_Essential_Libraries/PyFilesystem/walking_start.py
@@ -8,12 +8,46 @@ from fs.zipfs import ZipFS
 with OSFS(".") as myfs:
     print("-- Files --")
     # TODO: use the files walker to process files
+    # for path in myfs.walk.files():
+    for path in myfs.walk.files(filter=["*.txt"]):
+        print(path)
 
     print("-- Directories --")
     # TODO: use the dirs walker for directories
+    for path in myfs.walk.dirs():
+        print(path)
 
 # TODO: use the info property to step through items
+print("-- info --")
+with OSFS(".") as myfs:
+    for path, info in myfs.walk.info(namespaces=['details']):
+        print(path, info.is_dir, info.size)
+    
+# Calculat total file size
+with OSFS("FileExamples") as myfs:
+    total_size = 0
+    for path, info in myfs.walk.info(namespaces=['details']):
+        if path.endswith(".txt") and not info.is_dir:
+            total_size += info.size
+    
+    print("total size:", total_size)
+
 
 # TODO: Use the walk object by itself:
+print("-- steps --")
+# with OSFS(".") as myfs:
+with OSFS("FileExamples") as myfs:
+    for step in myfs.walk():
+        print(step.path)
+        print(step.files)
+        print(step.dirs)
 
 # TODO: Use the walker with a ZIP
+print("-- Zip steps --")
+with ZipFS("FileExamples.zip") as zipfs:
+    for step in zipfs.walk():
+        print(step.path)
+        print(step.files)
+        print(step.dirs)
+
+
