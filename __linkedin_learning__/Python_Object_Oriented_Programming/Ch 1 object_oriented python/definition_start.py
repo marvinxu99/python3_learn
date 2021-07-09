88a678835dda3cf1385c201bf2fbd983406e56a5
marvinxu99 <marvinxu99@hotmail.com>
2021-07-09 07:35:39 -0700

    added chapter names

--- __linkedin_learning__/Python_Object_Oriented_Programming/Ch 1 object_oriented python/definition_start.py
+++ __linkedin_learning__/Python_Object_Oriented_Programming/Ch 1 object_oriented python/definition_start.py
@@ -3,9 +3,14 @@
 
 
 # TODO: create a basic class
-
+class Book:
+    def __init__(self, title) -> None:
+        self.title = title
 
 # TODO: create instances of the class
-
+b1 = Book('Brave New World')
+b2 = Book('War and peace')
 
 # TODO: print the class and property
+print(b1)
+print(b2.title)
