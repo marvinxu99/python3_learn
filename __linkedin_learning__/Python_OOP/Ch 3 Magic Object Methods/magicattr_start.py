e4f39ce61eded59807687896848602d2daaf2a08
marvinxu99 <marvinxu99@hotmail.com>
2021-07-10 09:25:52 -0700

    get ot set attribute magic methods

--- __linkedin_learning__/Python_OOP/Ch 3 Magic Object Methods/magicattr_start.py
+++ __linkedin_learning__/Python_OOP/Ch 3 Magic Object Methods/magicattr_start.py
@@ -1,6 +1,6 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using the __str__ and __repr__ magic methods
-
+from typing import Any
 
 class Book:
     def __init__(self, title, author, price):
@@ -17,9 +17,19 @@ class Book:
 
     # TODO: __getattribute__ called when an attr is retrieved. Don't
     # directly access the attr name otherwise a recursive loop is created
+    def __getattribute__(self, name: str) -> Any:
+        if name == "price":
+            p = super().__getattribute__('price')
+            d = super().__getattribute__("_discount")
+            return p * (1 - d)
 
     # TODO: __setattr__ called when an attribute value is set. Don't set the attr
     # directly here otherwise a recursive loop causes a crash
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name == "price":
+            if type(value) not in (float, int):
+                raise ValueError("Value must be a float or int")
+        return super().__setattr__(name, value)
 
     # TODO: __getattr__ called when __getattribute__ lookup fails - you can
     # pretty much generate attributes on the fly with this method
@@ -27,3 +37,9 @@ class Book:
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
+
+print(f"{b1.price: .2f}")
+print(f"{b2.price: .2f}")
+
+b1.price = 38.5
+print(b1.price)
