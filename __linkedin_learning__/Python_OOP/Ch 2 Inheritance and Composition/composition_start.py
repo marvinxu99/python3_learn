ef9afab6794bfdb24e409f56b7c3c20902dac8d1
marvinxu99 <marvinxu99@hotmail.com>
2021-07-09 21:51:42 -0700

    composition examples

--- __linkedin_learning__/Python_OOP/Ch 2 Inheritance and Composition/composition_start.py
+++ __linkedin_learning__/Python_OOP/Ch 2 Inheritance and Composition/composition_start.py
@@ -3,23 +3,47 @@
 
 
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author):
         self.title = title
         self.price = price
 
-        self.authorfname = authorfname
-        self.authorlname = authorlname
+        self.author = author
 
         self.chapters = []
 
-    def addchapter(self, name, pages):
-        self.chapters.append((name, pages))
+    def addchapter(self, chapter):
+        self.chapters.append((chapter))
+
+    def getbookpagecount(self):
+        result = 0
+        for ch in self.chapters:
+            result += ch.pagecount
+        return result
 
 
-b1 = Book("War and Peace", 39.0, "Leo", "Tolstoy")
+class Author:
+    def __init__(self, fname, lname) -> None:
+        self.fname = fname
+        self.lname = lname
 
-b1.addchapter("Chapter 1", 125)
-b1.addchapter("Chapter 2", 97)
-b1.addchapter("Chapter 3", 143)
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
+
+class Chapter:
+    def __init__(self, name, pagecount) -> None:
+        self.name = name
+        self.pagecount = pagecount
+
+    def __str__(self):
+        return f"{self.name} {self.pagecount}"
+
+
+b1 = Book("War and Peace", 39.0, Author("Leo", "Tolstoy"))
+
+b1.addchapter(Chapter("Chapter 1", 125))
+b1.addchapter(Chapter("Chapter 2", 97))
+b1.addchapter(Chapter("Chapter 3", 143))
 
 print(b1.title)
+print(b1.getbookpagecount())
+print(b1.author)
