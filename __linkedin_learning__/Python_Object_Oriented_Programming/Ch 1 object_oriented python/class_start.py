755f5bddd24fb1a17ca1be2124760b6bc85d9204
marvinxu99 <marvinxu99@hotmail.com>
2021-07-09 17:03:15 -0700

    class method, static methods

--- __linkedin_learning__/Python_Object_Oriented_Programming/Ch 1 object_oriented python/class_start.py
+++ __linkedin_learning__/Python_Object_Oriented_Programming/Ch 1 object_oriented python/class_start.py
@@ -4,27 +4,45 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
 
     # TODO: double-underscore properties are hidden from other classes
+    __booklist = None
 
     # TODO: create a class method
+    @classmethod
+    def getbooktypes(cls):
+        return cls.BOOK_TYPES
 
     # TODO: create a static method
+    @staticmethod
+    def getbooklist():
+        if Book.__booklist == None:
+            Book.__booklist = []
+        return Book.__booklist
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def setTitle(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
+        if booktype not in Book.BOOK_TYPES:
+            raise ValueError(f"{booktype} is not a valid book type")
+        else:
+            self.booktype = booktype
 
 
 # TODO: access the class attribute
-
+print("Book types:", Book.getbooktypes())
 
 # TODO: Create some book instances
-b1 = Book("Brave New World")
-b2 = Book("War and Peace")
+b1 = Book("Brave New World", "HARDCOVER")
+b2 = Book("War and Peace", "EBOOK")
 
 # TODO: Use the static method to access a singleton object
+thebooks = Book.getbooklist()
+thebooks.append(b1)
+thebooks.append(b2)
+print(thebooks)
