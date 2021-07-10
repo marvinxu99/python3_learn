9096fec15bf7c8a91f3bac005831e9a800131395
marvinxu99 <marvinxu99@hotmail.com>
2021-07-10 11:41:04 -0700

    dataclass

--- __linkedin_learning__/Python_OOP/Ch 4 Data Class/immutable_start.py
+++ __linkedin_learning__/Python_OOP/Ch 4 Data Class/immutable_start.py
@@ -4,16 +4,21 @@
 from dataclasses import dataclass
 
 
-@dataclass()  # TODO: "The "frozen" parameter makes the class immutable
+# @dataclass()  # TODO: "The "frozen" parameter makes the class immutable
+@dataclass(frozen=True)  # TODO: "The "frozen" parameter makes the class immutable
 class ImmutableClass:
     value1: str = "Value 1"
     value2: int = 0
+
+    def some_func(self, newvalue):
+        self.value1 = newvalue
 
 
 obj = ImmutableClass()
 print(obj.value1)
 
 # TODO: attempting to change the value of an immutable class throws an exception
-
+# obj.value1 = "ANother value"
 
 # TODO: even functions within the class can't change anything
+obj.some_func("another value")
--- __linkedin_learning__/Python_OOP/Ch 4 Data Class/postinit_start.py
+++ __linkedin_learning__/Python_OOP/Ch 4 Data Class/postinit_start.py
@@ -1,7 +1,17 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using the postinit function in data classes
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+
+@dataclass
+class C:
+    a: float
+    b: float
+    c: float = field(init=False)
+
+    def __post_init__(self):
+        self.c = self.a + self.b
 
 
 @dataclass
@@ -13,10 +23,24 @@ class Book:
 
     # TODO: the __post_init__ function lets us customize additional properties
     # after the object has been initialized via built-in __init__
+    """
+    The generated __init__() code will call a method named __post_init__(), 
+    if __post_init__() is defined on the class. It will normally be called 
+    as self.__post_init__(). However, if any InitVar fields are defined, they 
+    will also be passed to __post_init__() in the order they were defined 
+    in the class. If no __init__() method is generated, 
+    then __post_init__() will not automatically be called.
+    """
+    def __post_init__(self):
+        self.description = f"{self.title} by {self.author}, {self.pages} pages "
 
 
-# create some Book objects
-b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
-b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+if __name__ == "__main__":
+    # create some instances
+    b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+    b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+    b3 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
 
-# TODO: use the description attribute
+    print(b3.description)
+    print(dir(b3))
+    print(b3)
