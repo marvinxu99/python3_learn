e3dd9317bc32c2f74544e92cb8457e08ffcea251
marvinxu99 <marvinxu99@hotmail.com>
2021-07-10 10:54:56 -0700

    call the object as if it were a function

--- __linkedin_learning__/Python_OOP/Ch 3 Magic Object Methods/magicattr_start.py
+++ __linkedin_learning__/Python_OOP/Ch 3 Magic Object Methods/magicattr_start.py
@@ -22,6 +22,7 @@ class Book:
             p = super().__getattribute__('price')
             d = super().__getattribute__("_discount")
             return p * (1 - d)
+        return super().__getattribute__(name)
 
     # TODO: __setattr__ called when an attribute value is set. Don't set the attr
     # directly here otherwise a recursive loop causes a crash
@@ -33,7 +34,8 @@ class Book:
 
     # TODO: __getattr__ called when __getattribute__ lookup fails - you can
     # pretty much generate attributes on the fly with this method
-
+    def __getattr__(self, name):
+        return name + " is not here"
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
@@ -43,3 +45,5 @@ print(f"{b2.price: .2f}")
 
 b1.price = 38.5
 print(b1.price)
+
+print(b1.fakename)
--- __linkedin_learning__/Python_OOP/Ch 3 Magic Object Methods/magiccall_start.py
+++ __linkedin_learning__/Python_OOP/Ch 3 Magic Object Methods/magiccall_start.py
@@ -1,6 +1,6 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using the __str__ and __repr__ magic methods
-
+from typing import Any
 
 class Book:
     def __init__(self, title, author, price):
@@ -13,9 +13,15 @@ class Book:
         return f"{self.title} by {self.author}, costs {self.price}"
 
     # TODO: the __call__ method can be used to call the object like a function
-
+    # def __call__(self, *args: Any, **kwds: Any) -> Any:
+    def __call__(self, title, author, price) -> Any:
+        self.title = title
+        self.author = author
+        self.price = price
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
 # TODO: call the object as if it were a function
+b1("War and Peace", "Leo Tolstoy", 40.95)
+print(b1)
