c89b55d2dcbc1e2ad66281be7507435679d2bd7a
marvinxu99 <marvinxu99@hotmail.com>
2021-07-09 19:59:20 -0700

    abstract base class

--- __linkedin_learning__/Python_OOP/Ch 2 Inheritance and Composition/abstract_start.py
+++ __linkedin_learning__/Python_OOP/Ch 2 Inheritance and Composition/abstract_start.py
@@ -1,11 +1,12 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using Abstract Base Classes to enforce class constraints
+from abc import ABC, abstractmethod    # abc - abstract base class
 
-
-class GraphicShape:
+class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod
     def calcArea(self):
         pass
 
@@ -14,13 +15,19 @@ class Circle(GraphicShape):
     def __init__(self, radius):
         self.radius = radius
 
+    def calcArea(self):
+        return 3.14 * (self.radius ** 2)
+
 
 class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
 
+    def calcArea(self):
+        return self.side ** 2
 
-g = GraphicShape()
+
+# g = GraphicShape()
 
 c = Circle(10)
 print(c.calcArea())
