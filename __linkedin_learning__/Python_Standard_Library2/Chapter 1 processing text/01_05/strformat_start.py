c389af73738cd5f835e9e719b2d96d40e86c28d3
marvinxu99 <marvinxu99@hotmail.com>
2021-07-11 13:45:59 -0700

    minor change

--- __linkedin_learning__/Python_Standard_Library2/Chapter 1 processing text/01_05/strformat_start.py
+++ __linkedin_learning__/Python_Standard_Library2/Chapter 1 processing text/01_05/strformat_start.py
@@ -1,16 +1,35 @@
 # String formatting methods and best practices
 
+from string import Template
+import datetime
 
 # TODO: Using Template strings
 the_str = "The quick brown $animal $action over the lazy dog"
+the_template = Template(the_str)
+output_str = the_template.substitute(animal='fox', action='jumps')
+print(output_str)
+
+args = {
+    'animal': 'cow',
+    'action': 'walked'
+}
+output_str = the_template.substitute(args)
+print(output_str)
 
 
 # TODO: Using str.format()
 foo = "foo"
 bar = 123
-
+print("output: {} {}".format(foo, bar))
+print("output: {1} {0}".format(foo, bar))
+print("output: {var1} {var2}".format(var1=foo, var2=bar))
+print("output: {var2:x}, {var2:X}, {var1}".format(var1=foo, var2=bar))
 
 # TODO: Using interpolation with f-strings in Python 3.6
 product = "Widget"
 price = 19.99
 tax = 0.07
+nyd = datetime.datetime(2020,1,1)
+
+print(f"{product} has a price of {price}, with tax {tax:.2%}, the total is {round(price *(1+tax), 2)}")
+print(f"But only on {nyd:%d %B %Y}")
