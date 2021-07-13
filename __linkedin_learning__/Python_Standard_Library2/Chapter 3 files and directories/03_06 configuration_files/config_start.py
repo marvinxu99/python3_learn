8d5cab5281930b048122a2de3c59afcb7ebd09ca
marvinxu99 <marvinxu99@hotmail.com>
2021-07-12 19:26:37 -0700

    ConfigParser example

--- __linkedin_learning__/Python_Standard_Library2/Chapter 3 files and directories/03_06 configuration_files/config_start.py
+++ __linkedin_learning__/Python_Standard_Library2/Chapter 3 files and directories/03_06 configuration_files/config_start.py
@@ -13,10 +13,27 @@ print(parser.sections())
 print(parser.has_section('Section 1'))
 
 # TODO: Access one of the default values
-
+use_time_travel = bool(parser['DEFAULT']['UseTimeTravel'])
+print(use_time_travel)
+print(type(use_time_travel))
 
 # TODO: Demonstrate the getXXX convenience functions
+use_time_travel = parser.getboolean('DEFAULT', 'UseTimeTravel')
+print(use_time_travel)
+print(type(use_time_travel))
 
+opd = parser.getboolean('DEFAULT', 'ObeyPrimeDirective')
+print(opd)
+
+dyj = parser['DEFAULT'].getint('DefaultYearJump')
+print(dyj, type(dyj))
+
+speed = parser.getfloat('DEFAULT', 'Ship Speed') 
+print(speed, type(speed))
 
 # TODO: Access a non-existent value
-
+try:
+    titile = parser['Section 3']['title']
+    print(titile)
+except KeyError as err:
+    print(err)
