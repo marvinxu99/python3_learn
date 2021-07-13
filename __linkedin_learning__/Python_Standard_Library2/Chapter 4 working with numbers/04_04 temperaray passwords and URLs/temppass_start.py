34cd489c2b305907e6288549529583703926ea41
marvinxu99 <marvinxu99@hotmail.com>
2021-07-12 21:48:48 -0700

    uuid

--- __linkedin_learning__/Python_Standard_Library2/Chapter 4 working with numbers/04_04 temperaray passwords and URLs/temppass_start.py
+++ __linkedin_learning__/Python_Standard_Library2/Chapter 4 working with numbers/04_04 temperaray passwords and URLs/temppass_start.py
@@ -5,19 +5,28 @@ import string
 
 # TODO: Function to return a temporary password given a length
 def generateTempPass(numChars=8):
-    pass
-
+    potential_chars = string.ascii_letters + string.digits + "=-&?/!@#$%*"
+    result = ''.join(secrets.choice(potential_chars) for _ in range(numChars))
+    print(result)
 
 # TODO: Function to return a temporary password and enforce 1 number and 1 uppercase
 def generateBetterPass(numChars=8):
-    pass
+    potential_chars = string.ascii_letters + string.digits + "=-&?/!@#$%*"
+    while True:
+        result = ''.join(secrets.choice(potential_chars) for _ in range(numChars))
+        if (any(c.isupper() for c in result) 
+                and any(c.isdigit() for c in result)):
+            break    
+    return result
 
 # create a temporary password
-# print(generateTempPass(10))
+print(generateTempPass(10))
 
 # create a stronger temporary password
-# print(generateBetterPass(10))
+print(generateBetterPass(10))
 
 
 # TODO: create a temporary, hard-to-guess URL
 resultUrl = "https://my.example.com?reset="
+resultUrl += secrets.token_urlsafe(15)
+print(resultUrl)
--- __linkedin_learning__/Python_Standard_Library2/Chapter 4 working with numbers/04_05 Generate unique identifiers/uuid_start.py
+++ __linkedin_learning__/Python_Standard_Library2/Chapter 4 working with numbers/04_05 Generate unique identifiers/uuid_start.py
@@ -4,11 +4,16 @@ import uuid
 
 # TODO: use the uuid4 function to create a random sequence using
 # the underlying os.urandom() function
-
+result = uuid.uuid4()
+print('UUID4:', result)
+print(result.hex)
+print(result.urn)
 print("~~~~~~~~~~~~~~~~~~~~~~~\n")
-
 
 # TODO: create a UUID using uuid5, which takes a namespace and
 # name value. Note that this version is not crypto-safe
-
+result = uuid.uuid5(uuid.NAMESPACE_DNS, 'example.com')
+print('UUID5:', result)
+print(result.hex)
+print(result.urn)
 print("~~~~~~~~~~~~~~~~~~~~~~~\n")
