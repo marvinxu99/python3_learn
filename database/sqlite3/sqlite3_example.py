3de560573a92e33daa85199d7df9c8d9f8ab52e4
marvinxu99 <marvinxu99@hotmail.com>
2021-07-15 19:02:53 -0700

    sqlites exmple

---
+++ database/sqlite3/sqlite3_example.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+connection = sqlite3.connect('movies.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS Movies
+        (Title TEXT, Director TEXT, Year INT)''')
+
+# cursor.execute("INSERT INTO Movies VALUES('Taxi Driver', 'Martin Scorsese', 1976)")
+
+cursor.execute("SELECT * FROM Movies")
+print(cursor.fetchone())
+
+famous_films = [
+    ('Pulp Fictin', 'Quentin Tarantino', 1994),
+    ('Back to the Future', 'Steven Spielberg', 1985),
+    ('Moonrise Kingdom', 'Wes Anderson', 2012)
+]
+# cursor.executemany('INSERT INTO Movies VALUES (?,?,?)', famous_films)
+
+records = cursor.execute("SELECT * FROM Movies")
+print(type(records))
+# print(cursor.fetchall())
+for record in records:
+     print(record)
+
+# Filter
+print("Filter...")
+release_year = (1985,)
+cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
+print(cursor.fetchall())
+
+connection.commit()
+connection.close()
